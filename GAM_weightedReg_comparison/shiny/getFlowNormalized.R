0cb626b92a2d8cb59984ef5133d127f8d2ed388a
sastoudt <sstoudt@berkeley.edu>
2016-06-06 18:45:33 -0400

    fix flow normalization, do annual tab

---
+++ GAM_weightedReg_comparison/shiny/getFlowNormalized.R
@@ -0,0 +1,66 @@
+## flow normalized
+
+getFlowNormalized <- function(mod_in, dat_in, flow,  allflo = T){
+  
+  # add year, month columns to dat_in
+  dat_in <- mutate(dat_in, 
+                   month = as.numeric(strftime(date, '%m')), 
+                   year = as.numeric(strftime(date, '%Y'))
+  )
+  to_plo <- dat_in
+  
+  # flo values to predict
+  flo_vals <- flow
+  
+  
+  # get model predictions across range of flow values
+  dynadat <- rep(flo_vals, each = nrow(to_plo)) %>%  ## 13020
+    matrix(., nrow = nrow(to_plo), ncol = 1) %>% ## 434 x 30
+    cbind(to_plo[, c('dec_time', 'doy')], .) %>% # 434 x 32
+    gather('split', 'flo', -dec_time, -doy) %>% ##13020 x 4
+    select(-split) %>%  ## 13020 x 3
+    data.frame(., res = predict(mod_in, .)) %>%  ## 13020 x 4
+    spread(flo, res) %>%  ## 434 x 32
+    select(-dec_time, -doy) ## 434 x 30
+  
+  # merge predictions with year, month data, make long format
+  to_plo <- select(to_plo, year, month) %>% 
+    cbind(., dynadat) %>%  ## 434 x 32
+    gather('flo', 'res', -year, -month) %>% ## 13020 x 4
+    mutate(flo = as.numeric(as.character(flo)))
+  
+  # constrain plots to salinity limits for the selected month
+  if(!allflo){
+    
+    #min, max salinity values to plot
+    lim_vals <- group_by(data.frame(dat_in), month) %>% 
+      summarise(
+        Low = quantile(flo, 0.05, na.rm = TRUE),
+        High = quantile(flo, 0.95, na.rm = TRUE)
+      )
+    ## do I need to do this to match? why would we want to do this in general?
+    ## default: don't do this for now
+
+    # merge limts with months
+    to_plo <- left_join(to_plo, lim_vals, by = 'month')
+
+    
+    # reduce data
+    sel_vec <- with(to_plo, 
+                    flo >= Low &
+                      flo <= High
+    )
+    to_plo <- to_plo[sel_vec, !names(to_plo) %in% c('Low', 'High')]
+    to_plo <- arrange(to_plo, year, month)
+    
+  }
+  
+  # reshape data frame, average by year, month for symmetry
+  to_plo <- group_by(to_plo, year, month, flo) %>% 
+    summarise(
+      res = mean(res, na.rm = TRUE)
+    )
+  
+ return(to_plo)
+  
+}
--- GAM_weightedReg_comparison/shiny/server.R
+++ GAM_weightedReg_comparison/shiny/server.R
@@ -1,3 +1,4 @@
+source("getFlowNormalized.R")
 lablk <- list(
   shrt = c('din', 'nh', 'no23'),
   lngs = c(
@@ -12,7 +13,7 @@ stalk <- list(
   lngs = c('San Joaquin', 'Sacramento', 'Salinity')
 )
 
-flowPlot_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F){
+flowPlot_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F,annual=F){
   ylabel=with(lablk, lngs[shrt == data$resdup[1]])
   if(scale){
     data$res=exp(data$res)
@@ -24,9 +25,36 @@ flowPlot_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F){
     
   }
   data=data[!is.na(data$res),]
-  data=data[!is.na(data$flo),]
-
+  data=data[!is.na(data$flo),] ## I think this is what might be causing the weird breaks
+  ## removing NAs before they get to be counted in the aggregation
   titleLab=ifelse(scale,data$resdup[1],paste("ln(",data$resdup[1],")",sep=""))
+  
+  if(annual){
+    forAgg3=as.data.frame(cbind.data.frame(data$date,data$res,mod$fitted.values))
+    forAgg4=as.data.frame(cbind.data.frame(data$date,data$res,modNoFlow$fitted.values))
+    names(forAgg3)=names(forAgg4)=c("date","res","fit")
+    data1=annual_agg(forAgg3,min_mo=11)
+    data2=annual_agg(forAgg4,min_mo=11)
+    data1=data1[order(data1$date),]
+    data2=data2[order(data2$date),]
+
+    # title
+    txt <- paste0(titleLab, ' ~ s(time) + s(season) + [s(flo)]') 
+    
+    ggplot(data1, aes(x = date, y = res))+geom_point()+
+      geom_line(data=data1,aes(y = fit, color = 'With Flow'),lwd=1)+
+      geom_line(data=data2,aes(y = fit, color = 'No Flow'), lwd=1)+
+      xlim(xlim)+
+      xlab("")+
+      ylab(ylabel)+
+      scale_colour_manual(name = '',
+                          labels = c('With Flow', 'No Flow'),
+                          values =c('darkblue','orange')
+      ) +
+      ggtitle(txt)
+    
+  }else{
+  
   
   # title
   txt <- paste0(titleLab, ' ~ s(time) + s(season) + [s(flo)]') 
@@ -43,64 +71,136 @@ flowPlot_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F){
     ) + 
   ggtitle(txt)
 }
-## need legend
+
+}
 
 
 
-
-flowPlotNorm_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F){
+flowPlotNorm_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F,annual=F){
   ylabel=with(lablk, lngs[shrt == data$resdup[1]])
-  if(scale){
-    data$res=exp(data$res)
-    mod$fitted.values=exp(mod$fitted.values)
-    modNoFlow$fitted.values=exp(modNoFlow$fitted.values)
-    ylabel <- gsub('ln-|log-', '', as.character(ylabel))
-    ylabel <- as.expression(parse(text = ylabel))
-
-  }
+  # if(scale){
+  #   data$res=exp(data$res)
+  #   mod$fitted.values=exp(mod$fitted.values)
+  #   modNoFlow$fitted.values=exp(modNoFlow$fitted.values)
+  #   ylabel <- gsub('ln-|log-', '', as.character(ylabel))
+  #   ylabel <- as.expression(parse(text = ylabel))
+  # 
+  # }
   data=data[!is.na(data$res),]
   data=data[!is.na(data$flo),]
-  data$month=as.numeric(unlist(lapply(data$date,function(x){format(x,"%m")})))
-  data$predVal=mod$fitted.values
-  data$predValNoFlow=modNoFlow$fitted.values
-  months=1:12
-  normalGrid=as.data.frame(months)
+  data$month=as.numeric(strftime(data$date, '%m'))
+  data$year=as.numeric(strftime(data$date, '%Y'))
+  monthly<-group_by(data,month)
+  monthFlow<-summarise(monthly,meanFlow=mean(flo,na.rm=T))
+  monthFlow=as.data.frame(monthFlow)
+  names(monthFlow)=c("month","avgFlow")
+
+  mergeData=merge(data,monthFlow,by.x="month",by.y="month")
   
-  normalGrid$normVal=rep(NA,nrow(normalGrid))
-  normalGrid$normValNoFlow=rep(NA,nrow(normalGrid))
-  names(normalGrid)[1]=c("month")
+  normVal=getFlowNormalized(mod,data,monthFlow$avgFlow)
+  normValNoFlow=getFlowNormalized(modNoFlow,data,monthFlow$avgFlow)
   
-  for(i in 1:nrow(normalGrid)){
+  normVal$date=as.Date(paste(normVal$year,normVal$month,"01",sep="-"))
+  normValNoFlow$date=as.Date(paste(normValNoFlow$year,normValNoFlow$month,"01",sep="-"))
+  normVal=normVal[order(normVal$date),]
+  normValNoFlow=normValNoFlow[order(normValNoFlow$date),]
+  #print(head(normValNoFlow))
+  #print(head(normVal))
+  data2=as.data.frame(cbind.data.frame(normVal$date,normVal$res,normValNoFlow$res,normVal$month,normVal$year))
+  names(data2)=c("date","normVal","normValNoFlow","month","year")
+  
+  mergeData2=merge(mergeData,data2,by.x=c("year","month"),by.y=c("year","month"))
+  # data$month=as.numeric(unlist(lapply(data$date,function(x){format(x,"%m")})))
+  # data$predVal=mod$fitted.values
+  # data$predValNoFlow=modNoFlow$fitted.values
+  # months=1:12
+  # normalGrid=as.data.frame(months)
+  # 
+  # normalGrid$normVal=rep(NA,nrow(normalGrid))
+  # normalGrid$normValNoFlow=rep(NA,nrow(normalGrid))
+  # names(normalGrid)[1]=c("month")
+  # 
+  # for(i in 1:nrow(normalGrid)){
+  #   
+  #   sub=subset(data,  month==normalGrid[i,1])
+  #   
+  #   normalGrid$normVal[i]=mean(sub$predVal,na.rm=T)
+  #   normalGrid$normValNoFlow[i]=mean(sub$predValNoFlow,na.rm=T)
+  #   #print(i)
     
-    sub=subset(data,  month==normalGrid[i,1])
+# }
+  titleLab=ifelse(scale,data$resdup[1],paste("ln(",data$resdup[1],")",sep=""))
+  
+  if(annual){
+    forAgg=mergeData2[,c("date.x","res","normVal")]
+    forAgg2=mergeData2[,c("date.x","res","normValNoFlow")]
+    names(forAgg)=names(forAgg2)=c("date","res","norm")
+    data1=annual_agg(forAgg,min_mo=11)
+    data2=annual_agg(forAgg2,min_mo=11)
     
-    normalGrid$normVal[i]=mean(sub$predVal,na.rm=T)
-    normalGrid$normValNoFlow[i]=mean(sub$predValNoFlow,na.rm=T)
-    #print(i)
+    if(scale){
+      data1$res=exp(data1$res)
+      data2$res=exp(data2$res)
+      data1$norm=exp(data1$norm)
+      data2$norm=exp(data2$norm)
+      
+      ylabel <- gsub('ln-|log-', '', as.character(ylabel))
+      ylabel <- as.expression(parse(text = ylabel))
+      
+    }
+    data1=data1[order(data1$date),]
+    data2=data2[order(data2$date),]
+    txt <- paste0("Flow Normalized: ",titleLab, ' ~ s(time) + s(season) + [s(flo)]') 
+    
+    ggplot(data1, aes(x = date, y = res))+geom_point()+
+      geom_line(aes(y = norm, color = 'With Flow'),lwd=1)+
+      geom_line(data=data2,aes(y = norm, color = 'No Flow'),lwd=1)+
+      xlim(xlim)+
+      xlab("")+
+      ylab(ylabel)+
+      scale_colour_manual(name = '',
+                          labels = c('With Flow', 'No Flow'),
+                          values =c('darkblue','orange')
+      ) +
+      ggtitle(txt)
     
   }
-  titleLab=ifelse(scale,data$resdup[1],paste("ln(",data$resdup[1],")",sep=""))
+
+else{
                   
   # title
-  txt <- paste0("Average Per Month Across Years: ",titleLab, ' ~ s(time) + s(season) + [s(flo)]') 
+  txt <- paste0("Flow Normalized: ",titleLab, ' ~ s(time) + s(season) + [s(flo)]') 
   
-  data=merge(data,normalGrid,by.x="month",by.y="month")
-  data=data[order(data$date),]
-  ggplot(data, aes(x = date, y = res))+geom_point()+
+  #data=merge(data,normalGrid,by.x="month",by.y="month")
+  
+  #data=data[order(data$date),]
+  data=mergeData2[order(mergeData2$date.x),]
+  
+  if(scale){
+    data$res=exp(data$res)
+    data$normVal=exp(data$normVal)
+    data$normValNoFlow=exp(data$normValNoFlow)
+    ylabel <- gsub('ln-|log-', '', as.character(ylabel))
+    ylabel <- as.expression(parse(text = ylabel))
+    
+  }
+  
+  
+  ggplot(data, aes(x = date.x, y = res))+geom_point()+
     geom_line(aes(y = normVal, color = 'With Flow'),lwd=1)+
     geom_line(aes(y = normValNoFlow, color = 'No Flow'),lwd=1)+
     xlim(xlim)+
     xlab("")+
     ylab(ylabel)+
-  scale_colour_manual(name = '', 
-    labels = c('With Flow', 'No Flow'), 
+  scale_colour_manual(name = '',
+    labels = c('With Flow', 'No Flow'),
     values =c('darkblue','orange')
-    ) + 
+    ) +
   ggtitle(txt)
   
 }
 
-
+}
 
 # packages to use
 library(mgcv)
@@ -231,7 +331,7 @@ shinyServer(function(input, output) {
     dat=dataNiceNoLag[[dat()]]
    
     # create plot
-    flowPlot_SAS(dat,modelsNoLag_NoFlow_default[[mod()]],modelsNoLag_default[[mod()]],xlim=dt_rng,scale=logspace)
+    flowPlot_SAS(dat,modelsNoLag_NoFlow_default[[mod()]],modelsNoLag_default[[mod()]],xlim=dt_rng,scale=logspace,annual=annuals)
    
   }, height = 250, width = 1200)
   
@@ -256,7 +356,7 @@ shinyServer(function(input, output) {
     ## wrtds
     
     dat=dataNiceNoLag[[dat()]]
-    flowPlotNorm_SAS(dat,modelsNoLag_NoFlow_default[[mod()]],modelsNoLag_default[[mod()]],xlim=dt_rng,scale=logspace)
+    flowPlotNorm_SAS(dat,modelsNoLag_NoFlow_default[[mod()]],modelsNoLag_default[[mod()]],xlim=dt_rng,scale=logspace,annual=annuals)
     
     
   }, height = 250, width = 1200)
