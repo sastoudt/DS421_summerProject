8d1a62286b8daa14c41b8b251db6a101b85314cc
sastoudt <sstoudt@berkeley.edu>
2016-06-07 20:22:30 -0400

    change default basis dimensions

--- GAM_weightedReg_comparison/shiny/setup_code/defaultNoLagModels.R
+++ GAM_weightedReg_comparison/shiny/setup_code/defaultNoLagModels.R
@@ -18,8 +18,8 @@ for(i in 1:length(dataNiceNoLag)){
     )
   tmp=tmp[!is.na(tmp$res),]
   tmp=tmp[!is.na(tmp$flo),]
-  gamDEFAULT <- gam(res ~ te(dec_time, doy, flo, bs = c("tp", "cc", "tp")), data = tmp)
-  gamDEFAULTnoflow <- gam(res ~ te(dec_time, doy, bs = c("tp", "cc")), data = tmp)
+  gamDEFAULT <- gam(res ~ te(dec_time, doy, flo, bs = c("tp", "cc", "tp"),k=c(6,6,6)), data = tmp)
+  gamDEFAULTnoflow <- gam(res ~ te(dec_time, doy, bs = c("tp", "cc"),k=c(15,15)), data = tmp)
   
 #  gamtmp <- gam(res ~ te(dec_time, doy, flo, bs = c("tp", "cc", "tp")), k = c(5, 8, 5), data = tmp, knots = list(doy = c(1, 366)))
   
@@ -30,5 +30,19 @@ for(i in 1:length(dataNiceNoLag)){
   print(i)
 }
 
-save(modelsNoLag_default,file="modelsNoLag_default.RData")
-save(modelsNoLag_NoFlow_default,file="modelsNoLag_NoFlow_default.RData")
+save(modelsNoLag_default,file="data/modelsNoLag_default.RData")
+save(modelsNoLag_NoFlow_default,file="data/modelsNoLag_NoFlow_default.RData")
+
+### check basis dimension
+for(i in 1:length(modelsNoLag_default)){
+  print(gam.check(modelsNoLag_default[[i]]))
+  
+}
+## change to k=c(6,6,6) from default
+## check manually, can't find a way to extract k' and edf from this output
+## highest edf in the 50s out of 99
+for(i in 1:length(modelsNoLag_NoFlow_default)){
+  print(gam.check(modelsNoLag_NoFlow_default[[i]]))
+  
+}
+## change to k=c(15,15) from default
