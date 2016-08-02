028291a8a68803430c86e79c0cdca97785907e0d
sastoudt <sstoudt@berkeley.edu>
2016-08-01 19:55:31 -0700

    pick out terms for model for each station, 2 stages, outline plan of attack

--- SFEI_chl/chooseVariables.R
+++ SFEI_chl/chooseVariables.R
@@ -27,7 +27,9 @@ plot(sfei$doy,sfei$bod)
 plot(sfei$date_dec,sfei$bod) ## so no data
 
 plot(sfei$doy,sfei$tkn)
+plot(sfei$doy,sfei$tkn,ylim=c(0,3)) ## monthly?
 plot(sfei$date_dec,sfei$tkn)
+
 
 plot(sfei$doy,sfei$ton)
 plot(sfei$date_dec,sfei$ton) ## so no data
@@ -80,7 +82,7 @@ plot(sfei$date_dec,sfei$pH,ylim=c(0,10)) ## missing data gap
 plot(sfei$doy,sfei$chl) ## spring/summerish
 plot(sfei$date_dec,sfei$chl) ## can see the peaks that we are getting in the data
 
-plot(sfei$doy,sfei$secchi,ylim=c(0,200)) ## montly structure
+plot(sfei$doy,sfei$secchi,ylim=c(0,200)) ## monthly structure
 plot(sfei$date_dec,sfei$secchi,ylim=c(0,200)) ## structure, need to get rid of clear outliers
 
 plot(sfei$doy,sfei$turb)
@@ -95,10 +97,52 @@ load("perStation.Rda")
 ## Procedure for Selecting Variables for Model:
 
 ## Look at correlations/trends for all stations (above). Pick a few that look good and are interesting.
+## Also add some other variables of interest in a "full model" for interpretation purposes.
 ## For each station, do a quick set of pairs plot to make sure the overall model makes sense and/or that
 ## we aren't missing any important variables for that station.
 ## Fit GAM, find appropriate k's.
-## Save
+## Keep increasing k until gets slow/get warnings. Then follow this advice:
+## (i) fit your model and extract the deviance residuals;
+##(ii) for each smooth term in your model, fit an equivalent, single, smooth to the residuals, using a
+##substantially increased k to see if there is pattern in the residuals that could potentially be explained
+##by increasing k
+## Save in named list for future use
 
 ## Display nested impact + fitted values in Shiny app like the one created for work with Marcus.
- 
+
+plot(sfei$bod,sfei$chl)
+plot(sfei$date_dec,sfei$bod) ## not consistent data
+
+plot(sfei$tkn,sfei$chl) ## not great
+plot(sfei$date_dec,sfei$tkn) ##
+
+plot(sfei$ton,sfei$chl)
+plot(sfei$date_dec,sfei$ton) ## not consistent data
+
+plot(sfei$do,sfei$chl) ## not great
+plot(sfei$date_dec,sfei$do)
+
+plot(sfei$pheo,sfei$chl) ## good
+plot(sfei$date_dec,sfei$pheo)
+
+plot(sfei$tp,sfei$chl) ## not great
+plot(sfei$date_dec,sfei$tp)
+
+plot(sfei$doy,sfei$chl) ## spring, summer (same with pheo) (temp could also do this?)
+plot(sfei$date_dec,sfei$chl) 
+
+### "parsimonious model"
+
+### look promising
+## doy 
+## date_dec
+## pheo
+## tn (monthly structure) 
+
+### Add these for "full model"
+
+### of interest
+## sio2
+## po4
+## tss
+## nh4
