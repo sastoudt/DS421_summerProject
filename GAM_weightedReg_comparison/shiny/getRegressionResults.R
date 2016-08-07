277bbc5c51533dc787915de46ff6a336b1788650
sastoudt <sstoudt@berkeley.edu>
2016-08-07 16:45:20 -0700

    fix reg summary to get significantly different than 1

--- GAM_weightedReg_comparison/shiny/getRegressionResults.R
+++ GAM_weightedReg_comparison/shiny/getRegressionResults.R
@@ -17,7 +17,11 @@ if(index %in% c(10:18)){
   
   betas=unname(coefficients(mod.lm))
   pVals=unname(summary(mod.lm)$coefficients[,4])
+  #isSignificant[2]=ifelse( confint(mod.lm,level=1-sigLevel)[2,1]>1 |  confint(mod.lm,level=1-sigLevel)[2,2]<1,1,0)
+  r1 <- lm(data$wrtdsPred ~1 + offset(data$gamPred)) 
+  pVals[2]=anova(mod.lm, r1)$"Pr(>F)"[2]
   isSignificant=ifelse(pVals<sigLevel,1,0)
+  
   
   predValGAM=data$gamPred
   predValWRTDS=data$wrtdsPred
@@ -32,6 +36,8 @@ if(index %in% c(10:18)){
   mod.annual1=lm(annual1PW~annual1PG)
   annualBetas1=unname(coefficients(mod.annual1))
   annualPVals1=unname(summary(mod.annual1)$coefficients[,4])
+  r1 <- lm(annual1PW ~1 + offset(annual1PG)) 
+  annualPVals1[2]=anova(mod.annual1, r1)$"Pr(>F)"[2]
   isSignificantAnnual1=ifelse(annualPVals1<sigLevel,1,0)
   
   annual2=subset(data,year<1990 & year>=1983)
@@ -41,6 +47,8 @@ if(index %in% c(10:18)){
   mod.annual2=lm(annual2PW~annual2PG)
   annualBetas2=unname(coefficients(mod.annual2))
   annualPVals2=unname(summary(mod.annual2)$coefficients[,4])
+  r1 <- lm(annual2PW ~1 + offset(annual2PG)) 
+  annualPVals2[2]=anova(mod.annual2, r1)$"Pr(>F)"[2]
   isSignificantAnnual2=ifelse(annualPVals2<sigLevel,1,0)
   
   annual3=subset(data,year<1997 & year>=1990)
@@ -50,6 +58,8 @@ if(index %in% c(10:18)){
   mod.annual3=lm(annual3PW~annual3PG)
   annualBetas3=unname(coefficients(mod.annual3))
   annualPVals3=unname(summary(mod.annual3)$coefficients[,4])
+  r1 <- lm(annual3PW ~1 + offset(annual3PG)) 
+  annualPVals3[2]=anova(mod.annual3, r1)$"Pr(>F)"[2]
   isSignificantAnnual3=ifelse(annualPVals3<sigLevel,1,0)
   
   
@@ -65,6 +75,8 @@ if(index %in% c(10:18)){
   mod.annual4=lm(annual4PW~annual4PG)
   annualBetas4=unname(coefficients(mod.annual4))
   annualPVals4=unname(summary(mod.annual4)$coefficients[,4])
+  r1 <- lm(annual4PW ~1 + offset(annual4PG)) 
+  annualPVals4[2]=anova(mod.annual4, r1)$"Pr(>F)"[2]
   isSignificantAnnual4=ifelse(annualPVals4<sigLevel,1,0)
   }
   
@@ -75,6 +87,8 @@ if(index %in% c(10:18)){
   mod.annual5=lm(annual5PW~annual5PG)
   annualBetas5=unname(coefficients(mod.annual5))
   annualPVals5=unname(summary(mod.annual5)$coefficients[,4])
+  r1 <- lm(annual5PW ~1 + offset(annual5PG)) 
+  annualPVals5[2]=anova(mod.annual5, r1)$"Pr(>F)"[2]
   isSignificantAnnual5=ifelse(annualPVals5<sigLevel,1,0)
   
   
@@ -85,6 +99,8 @@ if(index %in% c(10:18)){
   mod.seasonal1=lm(seasonal1PW~seasonal1PG)
   seasonalBetas1=unname(coefficients(mod.seasonal1))
   seasonalPVals1=unname(summary(mod.seasonal1)$coefficients[,4])
+  r1 <- lm(seasonal1PW ~1 + offset(seasonal1PG)) 
+  seasonalPVals1[2]=anova(mod.seasonal1, r1)$"Pr(>F)"[2]
   isSignificantSeasonal1=ifelse(seasonalPVals1<sigLevel,1,0)
   
   seasonal2=subset(data,month %in% c(4:6))
@@ -94,6 +110,8 @@ if(index %in% c(10:18)){
   mod.seasonal2=lm(seasonal2PW~seasonal2PG)
   seasonalBetas2=unname(coefficients(mod.seasonal2))
   seasonalPVals2=unname(summary(mod.seasonal2)$coefficients[,4])
+  r1 <- lm(seasonal2PW ~1 + offset(seasonal2PG)) 
+  seasonalPVals2[2]=anova(mod.seasonal2, r1)$"Pr(>F)"[2]
   isSignificantSeasonal2=ifelse(seasonalPVals2<sigLevel,1,0)
   
   seasonal3=subset(data,month %in% c(7:9))
@@ -103,6 +121,8 @@ if(index %in% c(10:18)){
   mod.seasonal3=lm(seasonal3PW~seasonal3PG)
   seasonalBetas3=unname(coefficients(mod.seasonal3))
   seasonalPVals3=unname(summary(mod.seasonal3)$coefficients[,4])
+  r1 <- lm(seasonal3PW ~1 + offset(seasonal3PG)) 
+  seasonalPVals3[2]=anova(mod.seasonal3, r1)$"Pr(>F)"[2]
   isSignificantSeasonal3=ifelse(seasonalPVals3<sigLevel,1,0)
   
   seasonal4=subset(data,month %in% c(10:12))
@@ -112,6 +132,8 @@ if(index %in% c(10:18)){
   mod.seasonal4=lm(seasonal4PW~seasonal4PG)
   seasonalBetas4=unname(coefficients(mod.seasonal4))
   seasonalPVals4=unname(summary(mod.seasonal4)$coefficients[,4])
+  r1 <- lm(seasonal4PW ~1 + offset(seasonal4PG)) 
+  seasonalPVals4[2]=anova(mod.seasonal4, r1)$"Pr(>F)"[2]
   isSignificantSeasonal4=ifelse(seasonalPVals4<sigLevel,1,0)
   
   flow1=subset(data,flo<quantile(data$flo,.25))
@@ -121,6 +143,8 @@ if(index %in% c(10:18)){
   mod.flow1=lm(flow1PW~flow1PG)
   flowBetas1=unname(coefficients(mod.flow1))
   flowPVals1=unname(summary(mod.flow1)$coefficients[,4])
+  r1 <- lm(flow1PW ~1 + offset(flow1PG)) 
+  flowPVals1[2]=anova(mod.flow1, r1)$"Pr(>F)"[2]
   isSignificantFlow1=ifelse(flowPVals1<sigLevel,1,0)
   
   flow2=subset(data,flo>=quantile(data$flo,.25) & flo<quantile(data$flo,0.5))
@@ -130,6 +154,8 @@ if(index %in% c(10:18)){
   mod.flow2=lm(flow2PW~flow2PG)
   flowBetas2=unname(coefficients(mod.flow2))
   flowPVals2=unname(summary(mod.flow2)$coefficients[,4])
+  r1 <- lm(flow2PW ~1 + offset(flow2PG)) 
+  flowPVals2[2]=anova(mod.flow2, r1)$"Pr(>F)"[2]
   isSignificantFlow2=ifelse(flowPVals2<sigLevel,1,0)
   
   flow3=subset(data,flo>=quantile(data$flo,.5) & flo<quantile(data$flo,0.75))
@@ -139,6 +165,8 @@ if(index %in% c(10:18)){
   mod.flow3=lm(flow3PW~flow3PG)
   flowBetas3=unname(coefficients(mod.flow3))
   flowPVals3=unname(summary(mod.flow3)$coefficients[,4])
+  r1 <- lm(flow3PW ~1 + offset(flow3PG)) 
+  flowPVals3[2]=anova(mod.flow3, r1)$"Pr(>F)"[2]
   isSignificantFlow3=ifelse(flowPVals3<sigLevel,1,0)
   
   flow4=subset(data,flo>=quantile(data$flo,.75) )
@@ -148,6 +176,8 @@ if(index %in% c(10:18)){
   mod.flow4=lm(flow4PW~flow4PG)
   flowBetas4=unname(coefficients(mod.flow4))
   flowPVals4=unname(summary(mod.flow4)$coefficients[,4])
+  r1 <- lm(flow4PW ~1 + offset(flow4PG)) 
+  flowPVals4[2]=anova(mod.flow4, r1)$"Pr(>F)"[2]
   isSignificantFlow4=ifelse(flowPVals4<sigLevel,1,0)
   
 betas=rbind(betas,annualBetas1,annualBetas2,annualBetas3,annualBetas4,annualBetas5,
