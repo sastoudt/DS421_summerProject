e95e2d86b99068b7e6a7504263c089145773e00d
sastoudt <sstoudt@berkeley.edu>
2016-08-19 16:47:27 -0700

    try to get shreve order, end up doing it manually

--- SFEI_chl/getInNetworkFormat.R
+++ SFEI_chl/getInNetworkFormat.R
@@ -52,3 +52,68 @@ sfeiAdjMatrix[13,6]=1 ## P8 --> D28A
 row.names(sfeiAdjMatrix)=row.names
 colnames(sfeiAdjMatrix)=col.names
 View(sfeiAdjMatrix)
+
+library(Matrix)
+
+sfeiAdjMatrix_sp <- Matrix(sfeiAdjMatrix, sparse = TRUE) 
+
+source("~/Desktop/DS421_summerProject/SFEI_chl/toSource.R")
+
+make_weights(sfeiAdjMatrix_sp) ## in right format
+
+adjacency = sfeiAdjMatrix_sp
+#adjacency_to_shreve <- function(adjacency){
+  trips        <- triplet(adjacency)$indices
+  shreve.order <- inverse.order <- vector("numeric", length = nrow(adjacency))
+  #   INVERSE ORDER PROVIDES THE NUMBER OF STREAM SEGMENTS
+  #   EACH SEGMENTS LIES UPSTREAM FROM THE SOURCE
+  # the root node is the segment that has no downstream neighbours, ie. the outlet
+  root.node    <- which(rowSums(adjacency) == 0)
+  if(length(root.node) > 1){
+    #bid_roots <- nchar(adjacency$rid_bid[root.node, 2]) ## which one has more going into it
+    root.node <- root.node[1]
+  }
+  # inverse.order is a vector that accumulates from root.node upstream
+  # each element indicates the 'height' in the network of each segment
+  inverse.order[root.node] <- 1 
+  # every element of inverse.order should be non-zero.  
+  # A zero might indicate a lake or something disconnected from the network
+  # to get the inverse.order, siply count the number of characters in the binaryID vector
+  #inverse.order <- nchar(adjacency$rid_bid[, 2])
+  inverse.order<-c(5,2,3,4,4,5,5,3,1,1,2,6,6)
+  for(i in 1:nrow(trips)) inverse.order[trips[i,1]] <- inverse.order[trips[i,2]] + 1
+  # sources is a vector indicating the segments that have no upstream neighbours, ie the sources of the stream
+  sources <- which(colSums(adjacency) == 0)
+  # shreve.order assigns a weight of 1 to these source segments
+  shreve.order[sources] <- 1
+  # remaining gives the network 'heights' of the non-source segments that remain
+  # in reverse order, since shreve.order will accumlate from the highest to the lowest in the network
+  remaining <- rev(sort(unique(inverse.order[-sources])))
+  # we now ignore the sources and simply populate the remaining shreve.orders
+  inverse.order[sources] <- NA
+  # need the row locations of 1's in adjacency
+  # since spam uses column compression, must transpose to extract these efficiently
+  tadjacency <- t(adjacency)
+  # populate elements of shreve.order in reverse order of network 'height' (larger number means 'higher')
+  for(i in 1:length(remaining)){
+    # identify all those segments with network height == remaining[i]
+    next.down <- which(inverse.order == remaining[i])
+    # cycle through each of next.down in turn and add up their upstream weights
+    # this makes sense because every segment with the same network height has weight indep. of the others
+    for(j in 1:length(next.down)){
+      #shreve.order[next.down[j]] <- sum(shreve.order[tadjacency[next.down[j],]@colindices])
+      shreve.order[next.down[j]] <- sum(shreve.order[unname(which(tadjacency[next.down[j],]==1))])
+      
+    }
+  }
+  # finally print the shreve.order
+  shreve.order
+## this can't be right
+## https://en.wikipedia.org/wiki/Strahler_number
+
+  ### do by hand
+  
+  shreve.order<-c(1,2,2,2,1,2,1,1,1,1,1,2,1)
+
+#weight       <- adjacency_to_shreve(adjacency = sfeiAdjMatrix_sp)
+
