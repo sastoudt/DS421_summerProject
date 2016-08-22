5fe34c9ca878e7b66c83a883ceaf66e64cc943c7
sastoudt <sstoudt@berkeley.edu>
2016-08-21 18:11:47 -0700

    fix rmse division issue in getSummaryDifference

--- GAM_weightedReg_comparison/shiny/getSummaryDifference.R
+++ GAM_weightedReg_comparison/shiny/getSummaryDifference.R
@@ -10,7 +10,7 @@ getSummaryDifference<-function(data,index){
   predValWRTDS=data$wrtdsPred
   
  avgDiff=(sum(predValWRTDS,na.rm=T)-sum(predValGAM))/sum(predValGAM)*100
- rmse=sqrt(sum((predValWRTDS-predValGAM)^2,na.rm=T))
+ rmse=sqrt(sum((predValWRTDS-predValGAM)^2,na.rm=T)/sum(!is.na(predValWRTDS-predValGAM)))
 
   data$month=as.numeric(strftime(data$date, '%m'))
   data$year=as.numeric(strftime(data$date, '%Y'))
@@ -20,35 +20,35 @@ getSummaryDifference<-function(data,index){
   annual1PW=predValWRTDS[annual1I]
   annual1PG=predValGAM[annual1I]
   annualAvgDiff1=(sum(annual1PW,na.rm=T)-sum(annual1PG))/sum(annual1PG)*100
-  annualRMSE1=sqrt(sum((annual1PW-annual1PG)^2,na.rm=T))
+  annualRMSE1=sqrt(sum((annual1PW-annual1PG)^2,na.rm=T)/sum(!is.na(annual1PW-annual1PG)))
   
   annual2=subset(data,year<1990 & year>=1983)
   annual2I=which(data$year<1990 & data$year>=1983)
   annual2PW=predValWRTDS[annual2I]
   annual2PG=predValGAM[annual2I]
   annualAvgDiff2=(sum(annual2PW,na.rm=T)-sum(annual2PG))/sum(annual2PG)*100
-  annualRMSE2=sqrt(sum((annual2PW-annual2PG)^2,na.rm=T))
+  annualRMSE2=sqrt(sum((annual2PW-annual2PG)^2,na.rm=T)/sum(!is.na(annual2PW-annual2PG)))
   
   annual3=subset(data,year<1997 & year>=1990)
   annual3I=which(data$year<1997 & data$year>=1990)
   annual3PW=predValWRTDS[annual3I]
   annual3PG=predValGAM[annual3I]
   annualAvgDiff3=(sum(annual3PW,na.rm=T)-sum(annual3PG))/sum(annual3PG)*100
-  annualRMSE3=sqrt(sum((annual3PW-annual3PG)^2,na.rm=T))
+  annualRMSE3=sqrt(sum((annual3PW-annual3PG)^2,na.rm=T)/sum(!is.na(annual3PW-annual3PG)))
   
   annual4=subset(data,year<2004 & year>=1997)
   annual4I=which(data$year<2004 & data$year>=1997)
   annual4PW=predValWRTDS[annual4I]
   annual4PG=predValGAM[annual4I]
   annualAvgDiff4=(sum(annual4PW,na.rm=T)-sum(annual4PG))/sum(annual4PG)*100
-  annualRMSE4=sqrt(sum((annual4PW-annual4PG)^2,na.rm=T))
+  annualRMSE4=sqrt(sum((annual4PW-annual4PG)^2,na.rm=T)/sum(!is.na(annual4PW-annual4PG)))
   
   annual5=subset(data,year>=2004) ## has 2 extra years
   annual5I=which( data$year>=2004)
   annual5PW=predValWRTDS[annual5I]
   annual5PG=predValGAM[annual5I]
   annualAvgDiff5=(sum(annual5PW,na.rm=T)-sum(annual5PG))/sum(annual5PG)*100
-  annualRMSE5=sqrt(sum((annual5PW-annual5PG)^2,na.rm=T))
+  annualRMSE5=sqrt(sum((annual5PW-annual5PG)^2,na.rm=T)/sum(!is.na(annual5PW-annual5PG)))
 
   
   seasonal1=subset(data,month %in% c(1:3))
@@ -56,56 +56,56 @@ getSummaryDifference<-function(data,index){
   seasonal1PW=predValWRTDS[seasonal1I]
   seasonal1PG=predValGAM[seasonal1I]
   seasonalAvgDiff1=(sum(seasonal1PW,na.rm=T)-sum(seasonal1PG))/sum(seasonal1PG)*100
-  seasonalRMSE1=sqrt(sum((seasonal1PW-seasonal1PG)^2,na.rm=T))
+  seasonalRMSE1=sqrt(sum((seasonal1PW-seasonal1PG)^2,na.rm=T)/sum(!is.na(seasonal1PW-seasonal1PG)))
   
   seasonal2=subset(data,month %in% c(4:6))
   seasonal2I=which(data$month %in% c(4:6))
   seasonal2PW=predValWRTDS[seasonal2I]
   seasonal2PG=predValGAM[seasonal2I]
   seasonalAvgDiff2=(sum(seasonal2PW,na.rm=T)-sum(seasonal2PG))/sum(seasonal2PG)*100
-  seasonalRMSE2=sqrt(sum((seasonal2PW-seasonal2PG)^2,na.rm=T))
+  seasonalRMSE2=sqrt(sum((seasonal2PW-seasonal2PG)^2,na.rm=T)/sum(!is.na(seasonal2PW-seasonal2PG)))
   
   seasonal3=subset(data,month %in% c(7:9))
   seasonal3I=which(data$month %in% c(7:9))
   seasonal3PW=predValWRTDS[seasonal3I]
   seasonal3PG=predValGAM[seasonal3I]
   seasonalAvgDiff3=(sum(seasonal3PW,na.rm=T)-sum(seasonal3PG))/sum(seasonal3PG)*100
-  seasonalRMSE3=sqrt(sum((seasonal3PW-seasonal3PG)^2,na.rm=T))
+  seasonalRMSE3=sqrt(sum((seasonal3PW-seasonal3PG)^2,na.rm=T)/sum(!is.na(seasonal3PW-seasonal3PG)))
   
   seasonal4=subset(data,month %in% c(10:12))
   seasonal4I=which(data$month %in% c(10:12))
   seasonal4PW=predValWRTDS[seasonal4I]
   seasonal4PG=predValGAM[seasonal4I]
   seasonalAvgDiff4=(sum(seasonal4PW,na.rm=T)-sum(seasonal4PG))/sum(seasonal4PG)*100
-  seasonalRMSE4=sqrt(sum((seasonal4PW-seasonal4PG)^2,na.rm=T))
+  seasonalRMSE4=sqrt(sum((seasonal4PW-seasonal4PG)^2,na.rm=T)/sum(!is.na(seasonal4PW-seasonal4PG)))
   
   flow1=subset(data,flo<quantile(data$flo,.25))
   flow1I=which(data$flo<quantile(data$flo,0.25))
   flow1PW=predValWRTDS[flow1I]
   flow1PG=predValGAM[flow1I]
   flowAvgDiff1=(sum(flow1PW,na.rm=T)-sum(flow1PG))/sum(flow1PG)*100
-  flowRMSE1=sqrt(sum((flow1PW-flow1PG)^2,na.rm=T))
+  flowRMSE1=sqrt(sum((flow1PW-flow1PG)^2,na.rm=T)/sum(!is.na(flow1PW-flow1PG)))
   
   flow2=subset(data,flo>=quantile(data$flo,.25) & flo<quantile(data$flo,0.5))
   flow2I=which(data$flo>=quantile(data$flo,0.25)& data$flo<quantile(data$flo,0.5))
   flow2PW=predValWRTDS[flow2I]
   flow2PG=predValGAM[flow2I]
   flowAvgDiff2=(sum(flow2PW,na.rm=T)-sum(flow2PG))/sum(flow2PG)*100
-  flowRMSE2=sqrt(sum((flow2PW-flow2PG)^2,na.rm=T))
+  flowRMSE2=sqrt(sum((flow2PW-flow2PG)^2,na.rm=T)/sum(!is.na(flow2PW-flow2PG)))
   
   flow3=subset(data,flo>=quantile(data$flo,.5) & flo<quantile(data$flo,0.75))
   flow3I=which(data$flo>=quantile(data$flo,0.5)& data$flo<quantile(data$flo,0.75))
   flow3PW=predValWRTDS[flow3I]
   flow3PG=predValGAM[flow3I]
   flowAvgDiff3=(sum(flow3PW,na.rm=T)-sum(flow3PG))/sum(flow3PG)*100
-  flowRMSE3=sqrt(sum((flow3PW-flow3PG)^2,na.rm=T))
+  flowRMSE3=sqrt(sum((flow3PW-flow3PG)^2,na.rm=T)/sum(!is.na(flow3PW-flow3PG)))
   
   flow4=subset(data,flo>=quantile(data$flo,.75) )
   flow4I=which(data$flo>=quantile(data$flo,0.75))
   flow4PW=predValWRTDS[flow4I]
   flow4PG=predValGAM[flow4I]
   flowAvgDiff4=(sum(flow4PW,na.rm=T)-sum(flow4PG))/sum(flow4PG)*100
-  flowRMSE4=sqrt(sum((flow4PW-flow4PG)^2,na.rm=T))
+  flowRMSE4=sqrt(sum((flow4PW-flow4PG)^2,na.rm=T)/sum(!is.na(flow4PW-flow4PG)))
 
   
   avgDiff=rbind(avgDiff,annualAvgDiff1,annualAvgDiff2,annualAvgDiff3,annualAvgDiff4,
