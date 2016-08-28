909b7b2763ca73bf6fec85fcba259a7bb9eebf4d
sastoudt <sstoudt@berkeley.edu>
2016-08-28 13:01:28 -0700

    check results for reasonability

--- SFEI_chl/getModelMatrixB.R
+++ SFEI_chl/getModelMatrixB.R
@@ -51,3 +51,54 @@ betaHat
 
 ## everything ran, does this make even a little bit of sense?
 
+forMap=allData[,c("Longitude","Latitude","Station")]
+forMap=unique(forMap)
+dim(forMap)
+
+require(ggplot2)
+testMerge=cbind(forMap,betaHat)
+
+ggplot(testMerge,aes(x = Longitude, y = Latitude,colour=betaHat,cex=2))+geom_point()+
+  ggtitle("smnet betas by Station")+scale_size(guide=F)
+
+load(file="mod1Spatial.RData")
+mod1$coefficients
+statNam=unlist(lapply(names(mod1$coefficients)[1:15],function(x){y=strsplit(x,"Station)");unlist(y)[2]}))
+statNam[1]="C10"
+
+statNam
+intercept1=as.data.frame(cbind(statNam,mod1$coefficients[1:15]))
+intercept1
+
+intercept1=intercept1[-c(1,10),]
+intercept1
+
+require(gridExtra)
+
+g1<-ggplot(testMerge,aes(x = Longitude, y = Latitude,colour=betaHat,cex=2))+geom_point()+
+  ggtitle("smnet betas by Station")+scale_size(guide=F)
+
+testMerge2=cbind(forMap,intercept1)
+names(testMerge2)[5]="int"
+testMerge2$int=as.numeric(as.character(testMerge2$int))
+g2<-ggplot(testMerge2,aes(x = Longitude, y = Latitude,colour=int,cex=2))+geom_point()+
+  ggtitle("Spatial Model 1 Intercept by Station")+scale_size(guide=F)
+
+grid.arrange(g1,g2)
+
+## get mean per station
+
+require(dplyr)
+
+by_station <- group_by(test, Station)
+chlAvg <- summarise(by_station,
+                   count = n(),
+                   avg = mean(chl, na.rm = TRUE))
+
+chlAvg
+
+cbind.data.frame(chlAvg$Station,chlAvg$avg,betaHat)
+
+order(chlAvg$avg)
+order(betaHat)
+order(testMerge2$int)
