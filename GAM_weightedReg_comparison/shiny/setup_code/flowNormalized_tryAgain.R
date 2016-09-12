f4ad49d8b880f84d028a2eb812edbd8328e501a7
sastoudt <sstoudt@berkeley.edu>
2016-09-11 17:41:18 -0700

    new flow normalized outline done, now to make function form

--- GAM_weightedReg_comparison/shiny/setup_code/flowNormalized_tryAgain.R
+++ GAM_weightedReg_comparison/shiny/setup_code/flowNormalized_tryAgain.R
@@ -30,4 +30,45 @@ summary(modelsNoLag_Nested[[1]])
 
 ## for each year, each month all flows for that month
 
+year=seq(min(data$year),max(data$year),by=1)
 
+month=1:12
+
+predGrid=expand.grid(year,month)
+
+predGrid=c()
+for(i in 1:12){
+  predGrid=rbind(predGrid,expand.grid(i,year,byMonth[[i]]))
+  
+}
+head(predGrid)
+dim(predGrid)
+
+View(predGrid)
+class(predGrid)
+names(predGrid)=c("month","year","flo")
+
+head(data)
+
+dec_time = dec_time(Date)[['dec_time']],
+doy = yday(Date)
+
+head(day(data$date),50) ## all the first of the month
+
+## flo, doy, dec_time
+predGrid$Date=as.Date(paste(predGrid$year,predGrid$month,"01",sep="-"))
+predGrid$dec_time=dec_time(predGrid$Date)[['dec_time']]
+predGrid$doy=yday(predGrid$Date)
+testPred=predict(modelsNoLag_Nested[[1]],predGrid)
+length(testPred)
+
+summary(testPred) ## more compact, to be expected?
+summary(data$flo) 
+
+predGrid$testPred=testPred
+
+byYearMonth=group_by(predGrid,month,year)
+avgOverFlow=summarise(byYearMonth,meanPred=mean(testPred,na.rm=T))
+avgOverFlow
+
+avgOverFlow$Date=as.Date(paste(avgOverFlow$year,avgOverFlow$month,"01",sep="-"))
