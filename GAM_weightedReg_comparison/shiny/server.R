71072375f6be7395ab4920ab88ddbf06a445634c
sastoudt <sstoudt@berkeley.edu>
2016-09-11 18:14:14 -0700

    make new flow normalization into function and change around shiny to use it

--- GAM_weightedReg_comparison/shiny/server.R
+++ GAM_weightedReg_comparison/shiny/server.R
@@ -1,4 +1,4 @@
-source("getFlowNormalized.R")
+source("setup_code/getFlowNormalized.R")
 source("nestedPlot.R")
 source("spatialResultsPlot.R")
 source("comparePlot.R")
@@ -226,6 +226,10 @@ flowPlot_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F,annual=F)
 
 
 flowPlotNorm_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F,annual=F){
+  
+  ## doesn't make sense to flow normalize if we don't put flow in the model
+  ## don't really need modNoFlow anymore
+  
   ylabel=with(lablk, lngs[shrt == data$resdup[1]])
   # if(scale){
   #   data$res=exp(data$res)
@@ -235,30 +239,32 @@ flowPlotNorm_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F,annua
   #   ylabel <- as.expression(parse(text = ylabel))
   # 
   # }
-  data=data[!is.na(data$res),]
-  data=data[!is.na(data$flo),]
-  data$month=as.numeric(strftime(data$date, '%m'))
-  data$year=as.numeric(strftime(data$date, '%Y'))
-  monthly<-group_by(data,month)
-  monthFlow<-summarise(monthly,meanFlow=mean(flo,na.rm=T))
-  monthFlow=as.data.frame(monthFlow)
-  names(monthFlow)=c("month","avgFlow")
-
-  mergeData=merge(data,monthFlow,by.x="month",by.y="month")
+  # data=data[!is.na(data$res),]
+  # data=data[!is.na(data$flo),]
+  # data$month=as.numeric(strftime(data$date, '%m'))
+  # data$year=as.numeric(strftime(data$date, '%Y'))
+  # monthly<-group_by(data,month)
+  # monthFlow<-summarise(monthly,meanFlow=mean(flo,na.rm=T))
+  # monthFlow=as.data.frame(monthFlow)
+  # names(monthFlow)=c("month","avgFlow")
+  # 
+  # mergeData=merge(data,monthFlow,by.x="month",by.y="month")
+  # 
+  #normVal=getFlowNormalized(mod,data,monthFlow$avgFlow)
+  normVal=getFlowNormalized(data,mod)
   
-  normVal=getFlowNormalized(mod,data,monthFlow$avgFlow)
-  normValNoFlow=getFlowNormalized(modNoFlow,data,monthFlow$avgFlow)
+  #normValNoFlow=getFlowNormalized(modNoFlow,data,monthFlow$avgFlow)
   
-  normVal$date=as.Date(paste(normVal$year,normVal$month,"01",sep="-"))
-  normValNoFlow$date=as.Date(paste(normValNoFlow$year,normValNoFlow$month,"01",sep="-"))
+  #normVal$date=as.Date(paste(normVal$year,normVal$month,"01",sep="-"))
+ # normValNoFlow$date=as.Date(paste(normValNoFlow$year,normValNoFlow$month,"01",sep="-"))
   normVal=normVal[order(normVal$date),]
-  normValNoFlow=normValNoFlow[order(normValNoFlow$date),]
+#  normValNoFlow=normValNoFlow[order(normValNoFlow$date),]
   #print(head(normValNoFlow))
   #print(head(normVal))
-  data2=as.data.frame(cbind.data.frame(normVal$date,normVal$res,normValNoFlow$res,normVal$month,normVal$year))
-  names(data2)=c("date","normVal","normValNoFlow","month","year")
+ # data2=as.data.frame(cbind.data.frame(normVal$date,normVal$res,normValNoFlow$res,normVal$month,normVal$year))
+  #names(data2)=c("date","normVal","normValNoFlow","month","year")
   
-  mergeData2=merge(mergeData,data2,by.x=c("year","month"),by.y=c("year","month"))
+  #mergeData2=merge(mergeData,data2,by.x=c("year","month"),by.y=c("year","month"))
   # data$month=as.numeric(unlist(lapply(data$date,function(x){format(x,"%m")})))
   # data$predVal=mod$fitted.values
   # data$predValNoFlow=modNoFlow$fitted.values
@@ -283,69 +289,87 @@ flowPlotNorm_SAS=function(data,mod,modNoFlow,xlim=range(data$date),scale=F,annua
   if(annual){
     forAgg=mergeData2[,c("date.x","res","normVal")]
     forAgg2=mergeData2[,c("date.x","res","normValNoFlow")]
-    names(forAgg)=names(forAgg2)=c("date","res","norm")
-    data1=annual_agg(forAgg,min_mo=11)
-    data2=annual_agg(forAgg2,min_mo=11)
+    
+    forAgg=data[,c("date","res")]
+    forAgg2=normVal[,c("Date","meanPred")]
+    names(forAgg)=names(forAgg2)=c("date","res")
+    ## right input for annual_agg
+    data=annual_agg(forAgg,min_mo=11)
+    normVal=annual_agg(forAgg2,min_mo=11)
     
     if(scale){
-      data1$res=exp(data1$res)
-      data2$res=exp(data2$res)
-      data1$norm=exp(data1$norm)
-      data2$norm=exp(data2$norm)
-      
+      #data1$res=exp(data1$res)
+      #data2$res=exp(data2$res)
+      #data1$norm=exp(data1$norm)
+      #data2$norm=exp(data2$norm)
+      data$res=exp(data$res)
+      normVal$meanPred=exp(normVal$meanPred)
       ylabel <- gsub('ln-|log-', '', as.character(ylabel))
       ylabel <- as.expression(parse(text = ylabel))
       
     }
-    data1=data1[order(data1$date),]
-    data2=data2[order(data2$date),]
-    txt <- paste0("Flow Normalized: ",titleLab, ' ~ s(time) + s(season) + [s(flo)]') 
+    #data1=data1[order(data1$date),]
+    #data2=data2[order(data2$date),]
+    txt <- paste0("Flow Normalized: ",titleLab, ' ~ s(time) + s(season) + s(flo)') 
     
-    ggplot(data1, aes(x = date, y = res))+geom_point()+
-      geom_line(aes(y = norm, color = 'With Flow'),lwd=1)+
-      geom_line(data=data2,aes(y = norm, color = 'No Flow'),lwd=1)+
-      xlim(xlim)+
+    ggplot(data,aes(x=date,y=res))+geom_point()+
+      geom_line(data=  normVal, aes(x=Date,y=meanPred))+ xlim(xlim)+
       xlab("")+
       ylab(ylabel)+
-      scale_colour_manual(name = '',
-                          labels = c('With Flow', 'No Flow'),
-                          values =c('darkblue','orange')
-      ) +
       ggtitle(txt)
+    
+    
+    # ggplot(data1, aes(x = date, y = res))+geom_point()+
+    #   geom_line(aes(y = norm, color = 'With Flow'),lwd=1)+
+    #   geom_line(data=data2,aes(y = norm, color = 'No Flow'),lwd=1)+
+    #   xlim(xlim)+
+    #   xlab("")+
+    #   ylab(ylabel)+
+    #   scale_colour_manual(name = '',
+    #                       labels = c('With Flow', 'No Flow'),
+    #                       values =c('darkblue','orange')
+    #   ) +
+    #   ggtitle(txt)
     
   }
 
 else{
                   
   # title
-  txt <- paste0("Flow Normalized: ",titleLab, ' ~ s(time) + s(season) + [s(flo)]') 
+  txt <- paste0("Flow Normalized: ",titleLab, ' ~ s(time) + s(season) + s(flo)') 
   
   #data=merge(data,normalGrid,by.x="month",by.y="month")
   
   #data=data[order(data$date),]
-  data=mergeData2[order(mergeData2$date.x),]
+ # data=mergeData2[order(mergeData2$date.x),]
   
   if(scale){
     data$res=exp(data$res)
     data$normVal=exp(data$normVal)
-    data$normValNoFlow=exp(data$normValNoFlow)
+    #data$normValNoFlow=exp(data$normValNoFlow)
     ylabel <- gsub('ln-|log-', '', as.character(ylabel))
     ylabel <- as.expression(parse(text = ylabel))
     
   }
   
-  
-  ggplot(data, aes(x = date.x, y = res))+geom_point()+
-    geom_line(aes(y = normVal, color = 'With Flow'),lwd=1)+
-    geom_line(aes(y = normValNoFlow, color = 'No Flow'),lwd=1)+
-    xlim(xlim)+
+  ggplot(data,aes(x=date,y=res))+geom_point()+
+    geom_line(data=  normVal, aes(x=Date,y=meanPred))+ xlim(xlim)+
     xlab("")+
     ylab(ylabel)+
-  scale_colour_manual(name = '',
-    labels = c('With Flow', 'No Flow'),
-    values =c('darkblue','orange')
-    ) +
-  ggtitle(txt)
+    ggtitle(txt)
+  
+  
+  # ggplot(data, aes(x = date.x, y = res))+geom_point()+
+  #   geom_line(aes(y = normVal, color = 'With Flow'),lwd=1)+
+  #   geom_line(aes(y = normValNoFlow, color = 'No Flow'),lwd=1)+
+  #   xlim(xlim)+
+  #   xlab("")+
+  #   ylab(ylabel)+
+  # scale_colour_manual(name = '',
+  #   labels = c('With Flow', 'No Flow'),
+  #   values =c('darkblue','orange')
+  #   ) +
+  # ggtitle(txt)
   
 }
 
--- GAM_weightedReg_comparison/shiny/setup_code/flowNormalized_tryAgain.R
+++ GAM_weightedReg_comparison/shiny/setup_code/flowNormalized_tryAgain.R
@@ -1,74 +1,105 @@
 #setwd("~/Desktop/EPA_GAMS")
-library(dplyr)
-library(tidyr)
-library(lubridate)
-library(WRTDStidal)
+# library(dplyr)
+# library(tidyr)
+# library(lubridate)
+# library(WRTDStidal)
+# 
+# setwd("~/Desktop/DS421_summerProject/GAM_weightedReg_comparison/shiny/data")
+# 
+# load("dataNice_nolag.Rdata")
+# load("modelsNoLag_Nested.RData")
+# 
+# data=dataNiceNoLag[[1]]
+# 
+# 
+# head(data)
+# data=data[!is.na(data$res),]
+# data=data[!is.na(data$flo),]
+# data$month=as.numeric(strftime(data$date, '%m'))
+# data$year=as.numeric(strftime(data$date, '%Y'))
+# head(data)
+# 
+# byMonth=vector("list",12)
+# 
+# for(i in 1:12){
+# mon=subset(data,month==i)
+# byMonth[[i]]=mon$flo
+# }
+# 
+# summary(modelsNoLag_Nested[[1]])
+# 
+# ## for each year, each month all flows for that month
+# 
+# year=seq(min(data$year),max(data$year),by=1)
+# 
+# month=1:12
+# 
+# predGrid=expand.grid(year,month)
+# 
+# predGrid=c()
+# for(i in 1:12){
+#   predGrid=rbind(predGrid,expand.grid(i,year,byMonth[[i]]))
+#   
+# }
+# head(predGrid)
+# dim(predGrid)
+# 
+# View(predGrid)
+# class(predGrid)
+# names(predGrid)=c("month","year","flo")
+# 
+# head(data)
+# 
+# #dec_time = dec_time(Date)[['dec_time']],
+# #doy = yday(Date)
+# 
+# head(day(data$date),50) ## all the first of the month
+# 
+# ## flo, doy, dec_time
+# predGrid$Date=as.Date(paste(predGrid$year,predGrid$month,"01",sep="-"))
+# predGrid$dec_time=dec_time(predGrid$Date)[['dec_time']]
+# predGrid$doy=yday(predGrid$Date)
+# testPred=predict(modelsNoLag_Nested[[1]],predGrid)
+# length(testPred)
+# 
+# summary(testPred) ## more compact, to be expected?
+# summary(data$flo) 
+# 
+# predGrid$testPred=testPred
+# 
+# byYearMonth=group_by(predGrid,month,year)
+# avgOverFlow=summarise(byYearMonth,meanPred=mean(testPred,na.rm=T))
+# avgOverFlow
+# 
+# avgOverFlow$Date=as.Date(paste(avgOverFlow$year,avgOverFlow$month,"01",sep="-"))
 
-setwd("~/Desktop/DS421_summerProject/GAM_weightedReg_comparison/shiny/data")
 
-load("dataNice_nolag.Rdata")
-load("modelsNoLag_Nested.RData")
-
-data=dataNiceNoLag[[1]]
-
-
-head(data)
-data=data[!is.na(data$res),]
-data=data[!is.na(data$flo),]
-data$month=as.numeric(strftime(data$date, '%m'))
-data$year=as.numeric(strftime(data$date, '%Y'))
-head(data)
-
-byMonth=vector("list",12)
-
-for(i in 1:12){
-mon=subset(data,month==i)
-byMonth[[i]]=mon$flo
-}
-
-summary(modelsNoLag_Nested[[1]])
-
-## for each year, each month all flows for that month
-
-year=seq(min(data$year),max(data$year),by=1)
-
-month=1:12
-
-predGrid=expand.grid(year,month)
-
-predGrid=c()
-for(i in 1:12){
-  predGrid=rbind(predGrid,expand.grid(i,year,byMonth[[i]]))
+flowNormalized=function(data,mod){
+  data=data[!is.na(data$res),]
+  data=data[!is.na(data$flo),]
+  data$month=as.numeric(strftime(data$date, '%m'))
+  data$year=as.numeric(strftime(data$date, '%Y'))
+  
+  byMonth=vector("list",12)
+  year=seq(min(data$year),max(data$year),by=1)
+  
+  predGrid=c()
+  for(i in 1:12){
+    mon=subset(data,month==i)
+    byMonth[[i]]=mon$flo
+    predGrid=rbind(predGrid,expand.grid(i,year,byMonth[[i]]))
+  }
+  names(predGrid)=c("month","year","flo")
+  
+  predGrid$Date=as.Date(paste(predGrid$year,predGrid$month,"01",sep="-"))
+  predGrid$dec_time=dec_time(predGrid$Date)[['dec_time']]
+  predGrid$doy=yday(predGrid$Date)
+  testPred=predict(mod,predGrid)
+  
+  byYearMonth=group_by(predGrid,month,year)
+  avgOverFlow=summarise(byYearMonth,meanPred=mean(testPred,na.rm=T))
+  avgOverFlow$Date=as.Date(paste(avgOverFlow$year,avgOverFlow$month,"01",sep="-"))
+  
+  return(avgOverFlow)
   
 }
-head(predGrid)
-dim(predGrid)
-
-View(predGrid)
-class(predGrid)
-names(predGrid)=c("month","year","flo")
-
-head(data)
-
-dec_time = dec_time(Date)[['dec_time']],
-doy = yday(Date)
-
-head(day(data$date),50) ## all the first of the month
-
-## flo, doy, dec_time
-predGrid$Date=as.Date(paste(predGrid$year,predGrid$month,"01",sep="-"))
-predGrid$dec_time=dec_time(predGrid$Date)[['dec_time']]
-predGrid$doy=yday(predGrid$Date)
-testPred=predict(modelsNoLag_Nested[[1]],predGrid)
-length(testPred)
-
-summary(testPred) ## more compact, to be expected?
-summary(data$flo) 
-
-predGrid$testPred=testPred
-
-byYearMonth=group_by(predGrid,month,year)
-avgOverFlow=summarise(byYearMonth,meanPred=mean(testPred,na.rm=T))
-avgOverFlow
-
-avgOverFlow$Date=as.Date(paste(avgOverFlow$year,avgOverFlow$month,"01",sep="-"))
