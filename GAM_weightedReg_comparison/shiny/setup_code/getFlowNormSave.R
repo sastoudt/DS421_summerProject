65844c9063ebecc041ea84e0c00e4c7e23904a04
sastoudt <sstoudt@berkeley.edu>
2016-09-13 21:21:46 -0700

    save data in weird format that seems to fix bug

--- GAM_weightedReg_comparison/shiny/setup_code/getFlowNormSave.R
+++ GAM_weightedReg_comparison/shiny/setup_code/getFlowNormSave.R
@@ -4,21 +4,28 @@ setwd("~/Desktop/DS421_summerProject/GAM_weightedReg_comparison/shiny/data")
 
 load("dataNice_nolag.Rdata")
 load("modelsNoLag_Nested.RData")
-
+require(lubridate)
+require(WRTDStidal)
+require(dplyr)
 source("~/Desktop/DS421_summerProject/GAM_weightedReg_comparison/shiny/setup_code/flowNormalized_tryAgain.R")
 
 flowNormToSave=vector("list",length(dataNiceNoLag))
+flowNormToSave2=vector("list",length(dataNiceNoLag))
 for(i in 1:length(dataNiceNoLag)){
   
   data=dataNiceNoLag[[i]]
   mod=modelsNoLag_Nested[[i]]
   flowNorm=flowNormalized(data,mod)
   
-  test=cbind.data.frame(data$date,data$res,flowNorm) ## not same dim
-  names(test)=c("dateD","resD","dateA","resA")
+  test1=cbind.data.frame(data$date,data$res) 
+  test2=flowNorm
+  names(test1)=c("dateD","resD")
+  names(test2)=c("dateA","resA")
   
-  flowNormToSave[[i]]=test
+  flowNormToSave[[i]]=test1
+  flowNormToSave2[[i]]=test2
   print(i)
 }
 
 save(flowNormToSave,file="flowNormToLoad.RData")
+save(flowNormToSave2,file="flowNormToLoad2.RData")
